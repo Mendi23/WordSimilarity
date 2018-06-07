468aa476969403ab536c6321b16c28f20af4b691
mendi23 <18370401+Mendi23@users.noreply.github.com>
2018-06-07 19:02:34 +0300

    idiotic

--- word2vec/word2vec_examples.py
+++ word2vec/word2vec_examples.py
@@ -2,14 +2,10 @@
 import numpy as np
 from calculate import example_words
 
+
 def load_and_normalize_vectors(infile):
     W = []
     words = []
-    lines = 0
-    with open(infile, 'rb') as fileref:
-        for line in fileref:
-            lines += 1
-    lines = float(lines)
     with open(infile, 'rb') as fileref:
         for count, line in enumerate(fileref):
             line = line.split()
@@ -19,23 +15,27 @@ def load_and_normalize_vectors(infile):
             W.append(new_arr)
     return np.array(W), np.array(words)
 
+
 def print_results(filePath):
     with open(filePath, "w", encoding="utf8") as f:
         for example in example_words:
-            f.write("-"*80 + "\n")
+            f.write("-" * 80 + "\n")
             f.write(f"{example.decode('utf8'):>35}\n")
-            f.write("-"*80 + "\n")
+            f.write("-" * 80 + "\n")
             for x in _get_neighbours_iter(example):
                 f.write(" | ".join(f"{y.decode('utf8'):<14}" for y in x))
                 f.write("\n")
 
+
 def _get_neighbours_iter(example):
-        return zip(*(_get_neighbours(W, words, w2i, example, 20) for W, words, w2i in params))
+    return zip(*(_get_neighbours(W, words, w2i, example, 20) for W, words, w2i in params))
+
 
 def _get_neighbours(W, words, w2i, token, num_similiar):
     sim = W.dot(W[w2i[token]])
-    simIds = sim.argsort()[-1:num_similiar*-1:-1]
+    simIds = sim.argsort()[-1:num_similiar * -1:-1]
     return words[simIds]
+
 
 if __name__ == '__main__':
     W_d, words_d = load_and_normalize_vectors('deps.words')
